a72ea619775b51eb4491d4a47def23048f5beea3
Ryland Quirk <quirk2580@gmail.com>
2021-04-03 10:33:52 -0500

    testing some MongoDB items

--- models/book.js
+++ models/book.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  platform: { type: String, required: true },
   synopsis: String,
   date: { type: Date, default: Date.now }
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const Game = mongoose.model("Book", bookSchema);
 
-module.exports = Book;
+module.exports = Game;
