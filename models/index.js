8cf8008fd58e24de047c64aa2d46ed8984406ff1
Ryland Quirk <quirk2580@gmail.com>
2021-04-07 21:44:49 -0500

    model/route bug fix

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  Book: require("./book"), 
+  //Book: require("./book"), 
   VideoGames: require("./VideoGames")
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+//const bookRoutes = require("./books");
 const videoGameRoutes = require("./videoGame");
 
 // Book routes
-router.use("/books", bookRoutes);
+//router.use("/books", bookRoutes);
 router.use("/videoGames", videoGameRoutes);
 
 
