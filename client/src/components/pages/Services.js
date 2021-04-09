49893e11c604aa2e4ec88ea184213bffcc121de7
Chidi Ekeke <chidiekeke18@yahoo.com>
2021-04-09 12:33:09 -0500

    added useless li tag

--- client/src/components/pages/Services.js
+++ client/src/components/pages/Services.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import '../../App.css'
 
+
+//wishlist game display
 export default function Services () {
-    return <h1 className='services'>Wishlist</h1>
+    
+    return <h1 className='services'>Wishlist
+    <li>wishlist save</li>
+    </h1>
+    
 }
