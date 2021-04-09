86b529da07628ec90c88565ffb4b0545f9c182a1
Calvin Carter <calvincarter@pingidentity.com>
2021-04-09 16:40:15 -0500

    add card layout to services

--- client/src/components/pages/Services.js
+++ client/src/components/pages/Services.js
@@ -34,10 +34,35 @@ export default function Services() {
                 <div className="cards__wrapper">
                     <ul className="cards__items-container">
                         {/* <CardItem displayGames={favoriteGames} /> */}
-                        {/* ^^^^ the following seems to not show on page. I believe its due to CSS issue */}
+                        {/* ^^^^ the following seems to not show on page. I believe its due to CSS issue. Quickfix is just include your html here */}
+                        <li className="cards__item row">
                         {favoriteGames.map(game =>
-                            <p key={game.key}>{game.name}</p>
+                            <div className="cards__item__link col-md-4 col-sm-12 mt-3 p-4" key={game.id}>
+                            <figure className="cards__item__pic-wrap" data-category={game.label}>
+                              <img src={game.background_image}
+                                alt=""
+                                className="cards__item__img"
+                                images={game.background_image}
+                              />
+                            </figure>
+                            <div className="cards__item__info">
+                              <ul className="logo" className="fab fa-xbox">{game.logo}
+                                <li className="logo" className="fab fa-playstation"></li>
+                                <li className="logo" className="fab fa-windows"></li>
+              
+                              </ul>
+                              <h5 className="cards__item__text">{game.name}</h5>
+                              <h6 className="cards__item__text">{game.released}</h6>
+              
+                              <h6 className="cards__item__text">{game.releaseDate}
+                                <i className="fas fa-star">{game.rating}</i>
+                              </h6>
+                              <h6 className="cards__item__text">{game.ratingNumber}
+                              </h6>
+                            </div>
+                          </div>
                         )}
+                        </li>
                     </ul>
                 </div>
             </div>
